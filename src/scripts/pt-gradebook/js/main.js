6bb237e4c4ff9940274d1b172bdeb849fa16e645
Nathan Jones <Purkinje90@gmail.com>
2015-11-02 16:57:19 -0700

    -Added super-secret new launch on Meals link in Powerteacher -added eslintRules to package json (might be moved to .eslintrc soon)

--- src/scripts/pt-gradebook/js/main.js
+++ src/scripts/pt-gradebook/js/main.js
@@ -1,27 +1,28 @@
-/*global $j*/
 $j(document).ready(function () {
     'use strict';
-    var newLaunch = $j('#ptgNewLaunchBtn');
-    var oldLaunch = $j('#icon-btn-gblaunch');
-    var newLaunchBox = $j('#ptgNewLaunchBox');
+    if (window.location.pathname.indexOf('lunchtransactions.html') === -1) {
+      var newLaunch = $j('#ptgNewLaunchBtn');
+      var oldLaunch = $j('#icon-btn-gblaunch');
+      var newLaunchBox = $j('#ptgNewLaunchBox');
 
-    // Set new Launch button to old Launch link's href.
-    newLaunch.attr('href', oldLaunch.attr('href'));
+      // Set new Launch button to old Launch link's href.
+      newLaunch.attr('href', oldLaunch.attr('href'));
 
-    // Remove "Run Installer Once, then" text
-    newLaunchBox.find('p').eq(0).remove();
+      // Remove "Run Installer Once, then" text
+      newLaunchBox.find('p').eq(0).remove();
 
-    // Remove hr separator
-    newLaunchBox.find('hr').remove();
+      // Remove hr separator
+      newLaunchBox.find('hr').remove();
 
-    // Remove last p tag that contains "Or use the old Launch" text
-    // Because one p tag was removed, the p tag that we're removing here becomes the second (index: 1)
-    // p tag.
-    newLaunchBox.find('p').eq(1).remove();
+      // Remove last p tag that contains "Or use the old Launch" text
+      // Because one p tag was removed, the p tag that we're removing here becomes the second (index: 1)
+      // p tag.
+      newLaunchBox.find('p').eq(1).remove();
 
-    // Remove Gradebook page
-    $j('[href^="/teachers/gradebook/home.html"]').parent().remove();
+      // Remove Gradebook page
+      $j('[href^="/teachers/gradebook/home.html"]').parent().remove();
 
-    // Rename Gradebook Launch header
-    $j('#ptgNewLaunchBox').find('h2').text('GradeBook Launch');
+      // Rename Gradebook Launch header
+      $j('#ptgNewLaunchBox').find('h2').text('GradeBook Launch');
+    }
 });
