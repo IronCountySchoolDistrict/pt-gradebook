c5079612c66c871df87a4f9dcf4407c3ebd5bc6b
Issac Gable <zombiesix@zombiesix.local>
2016-06-30 08:37:56 -0600

    Removed old gradebook link

--- src/scripts/pt-gradebook/js/main.js
+++ src/scripts/pt-gradebook/js/main.js
@@ -1,28 +1,11 @@
 $j(document).ready(function () {
     'use strict';
     if (window.location.pathname.indexOf('lunchtransactions.html') === -1) {
-      var newLaunch = $j('#ptgNewLaunchBtn');
-      var oldLaunch = $j('#icon-btn-gblaunch');
       var newLaunchBox = $j('#ptgNewLaunchBox');
 
-      // Set new Launch button to old Launch link's href.
-      newLaunch.attr('href', oldLaunch.attr('href'));
-
-      // Remove "Run Installer Once, then" text
-      newLaunchBox.find('p').eq(0).remove();
-
-      // Remove hr separator
-      newLaunchBox.find('hr').remove();
-
-      // Remove last p tag that contains "Or use the old Launch" text
-      // Because one p tag was removed, the p tag that we're removing here becomes the second (index: 1)
-      // p tag.
-      newLaunchBox.find('p').eq(1).remove();
-
-      // Remove Gradebook page
-      $j('[href^="/teachers/gradebook/home.html"]').parent().remove();
-
+      // Removes Older Launch link
+      newLaunchBox.find('p').eq(2).remove();
       // Rename Gradebook Launch header
-      $j('#ptgNewLaunchBox').find('h2').text('GradeBook Launch');
+      $j('#ptgNewLaunchBox').find('h2').text('Old Gradebook');
     }
 });
