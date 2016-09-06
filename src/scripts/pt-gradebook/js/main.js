181df669f4df839ff3d1e99c2f292550b71b2076
Nathan Jones <nathan.jones@ironmail.org>
2016-09-06 11:37:36 -0600

    Use es6 module syntax to import jquery

--- src/scripts/pt-gradebook/js/main.js
+++ src/scripts/pt-gradebook/js/main.js
@@ -1,11 +1,14 @@
-$j(document).ready(function () {
-    'use strict';
-    if (window.location.pathname.indexOf('lunchtransactions.html') === -1) {
-      var newLaunchBox = $j('#ptgNewLaunchBox');
+import $ from 'jquery';
 
-      // Removes Older Launch link
-      newLaunchBox.find('p').eq(2).remove();
-      // Rename Gradebook Launch header
-      $j('#ptgNewLaunchBox').find('h2').text('Old Gradebook');
-    }
-});
+export default function() {
+  $(() => {
+      if (window.location.pathname.indexOf('lunchtransactions.html') === -1) {
+        var newLaunchBox = $('#ptgNewLaunchBox');
+
+        // Removes Older Launch link
+        newLaunchBox.find('p').eq(2).remove();
+        // Rename Gradebook Launch header
+        $('#ptgNewLaunchBox').find('h2').text('Old Gradebook');
+      }
+  });
+}
